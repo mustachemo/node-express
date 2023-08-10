5cdbe469a0ce6f6dee34cd865ea293afc4c55bf5
mustachemo <mhasan1080@gmail.com>
2023-08-09 23:38:16 -0700

    improved readability

--- src/controllers/getUsersController.js
+++ src/controllers/getUsersController.js
@@ -8,11 +8,12 @@ const postUsers = async (req, res) => {
   try {
     const submittedName = await Customer.find({ name: `${req.body.name}` });
     res.render('getUsers', {
-      message: `Successfully Retrieved user: ${submittedName[0].name}!`,
+      name: `Successfully Retrieved user: ${submittedName[0].name}!`,
+      age: `Age: ${submittedName[0].age}`,
     });
   } catch (error) {
     res.status(500).render('getUsers', {
-      message: `Failed to Retrieve user: ${req.body.name}!`,
+      name: `Failed to Retrieve user: ${req.body.name}!`,
     });
   }
 };
